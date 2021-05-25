9db9e3e0d0ce37b9a58af34a82a511e8362fe9e4
Max Truxa <dev@maxtruxa.com>
2021-05-25 23:52:21 +0200

    feat(zsh): Disable the SHARE_HISTORY option

--- .config/zsh/zshrc.d/20options.zsh
+++ .config/zsh/zshrc.d/20options.zsh
@@ -2,3 +2,7 @@
 unsetopt BEEP
 unsetopt HIST_BEEP
 unsetopt LIST_BEEP
+
+# Do not share the history between multiple shell instances. This is enabled by
+# default by Oh My Zsh.
+unsetopt SHARE_HISTORY
